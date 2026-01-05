e5de6a6474474349fb5683c361740e44347872b8
Ahmad Bagheri <u.ahmadbagheri@gmail.com>
2026-01-05 20:00:34 +0330

    [fix] rotation when moving

--- Assets/1_Ahmad/Scripts/Core/Player/Player Movement.cs
+++ Assets/1_Ahmad/Scripts/Core/Player/Player Movement.cs
@@ -6,8 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     private InputHandler _inputHandler;
     private CharacterController _characterController;
+    [SerializeField] private Transform _playerShape;
     [SerializeField] private float moveSpeed = 5f;
-
+    [SerializeField] private float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
     private void Start()
     {
         _inputHandler = GetComponent<InputHandler>();
@@ -17,12 +19,22 @@ public class PlayerMovement : MonoBehaviour
     private void HandleMovement()
     {
         Vector2 moveInput = _inputHandler.move;
-        Vector3 moveDirection = new Vector3(moveInput.x, 0, moveInput.y);
-        _characterController.Move(moveDirection * Time.deltaTime * moveSpeed);
+        Vector3 moveDirection = new(moveInput.x, 0, moveInput.y);
+        _characterController.Move(moveSpeed * Time.deltaTime * moveDirection);
+    }
+    private void HandleRotation()
+    {
+        if(_inputHandler.move.magnitude == 0) return;
+        float targetAngle = Mathf.Atan2(_inputHandler.move.x, _inputHandler.move.y) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0, angle, 0);
     }
 
     private void Update()
     {
         HandleMovement();
+        HandleRotation();
     }
+
+
 }
